7b6a38288162ea755bd97315cae38a5204174bb8
amangupta02004 <amangupta02004@gmail.com>
2024-07-27 11:15:04 +0530

    webaman

---
+++ unique/project/projectapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import authenticate
+
+
+admin.site.register(authenticate)
---
+++ unique/project/projectapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+
+    path('index', views.index, name='home'),
+    path('', views.user_login, name='login'),
+    path('signup/', views.user_signup, name='signup'),
+    path('logout/', views.user_logout, name='logout'),
+]
